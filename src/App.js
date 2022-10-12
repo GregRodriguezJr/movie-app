d79959f06e2a435d1b573a07a632bd2b5797d887
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-12 11:22:55 -0500

    hardcoded movie objects to test rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import MovieList from './components/movieList';
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([
+    {
+      "adult": false,
+      "backdrop_path": "/oTPSjQyETTFYDehdbiG8yi2oDmL.jpg",
+      "genre_ids": [
+          28,
+          16,
+          878,
+          10751
+      ],
+      "id": 13640,
+      "original_language": "en",
+      "original_title": "Superman: Doomsday",
+      "overview": "When LexCorp accidentally unleashes a murderous creature, Superman meets his greatest challenge as a champion. Based on the \"The Death of Superman\" storyline that appeared in DC Comics' publications in the 1990s.",
+      "popularity": 64.839,
+      "poster_path": "/itvuWm7DFWWzWgW0xgiaKzzWszP.jpg",
+      "release_date": "2007-09-18",
+      "title": "Superman: Doomsday",
+      "video": false,
+      "vote_average": 6.7,
+      "vote_count": 504
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/v6MVBFnQOscITvmAy5N5ras2JKZ.jpg",
+      "genre_ids": [
+          878,
+          28,
+          12
+      ],
+      "id": 1924,
+      "original_language": "en",
+      "original_title": "Superman",
+      "overview": "Mild-mannered Clark Kent works as a reporter at the Daily Planet alongside his crush, Lois Lane. Clark must summon his superhero alter-ego when the nefarious Lex Luthor launches a plan to take over the world.",
+      "popularity": 40.771,
+      "poster_path": "/d7px1FQxW4tngdACVRsCSaZq0Xl.jpg",
+      "release_date": "1978-12-13",
+      "title": "Superman",
+      "video": false,
+      "vote_average": 7.1,
+      "vote_count": 3141
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/v6MVBFnQOscITvmAy5N5ras2JKZ.jpg",
+      "genre_ids": [
+          878,
+          28,
+          12
+      ],
+      "id": 1924,
+      "original_language": "en",
+      "original_title": "Superman",
+      "overview": "Mild-mannered Clark Kent works as a reporter at the Daily Planet alongside his crush, Lois Lane. Clark must summon his superhero alter-ego when the nefarious Lex Luthor launches a plan to take over the world.",
+      "popularity": 40.771,
+      "poster_path": "/d7px1FQxW4tngdACVRsCSaZq0Xl.jpg",
+      "release_date": "1978-12-13",
+      "title": "Superman",
+      "video": false,
+      "vote_average": 7.1,
+      "vote_count": 3141
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MovieList movies = {movies} />
     </div>
   );
 }
