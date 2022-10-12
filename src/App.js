abee6189eb06015c2b4ccec240f9cc3c6f135fd1
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-12 13:32:50 -0500

    added two components and styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import MovieList from './components/movieList';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState([]);
 
   const getSearchedMovies = async () => {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_DB_Key}&language=en-US&page=1&include_adult=false&query=batman`;
@@ -14,11 +17,15 @@ const App = () => {
   }
 
   useEffect(() => {
-    getSearchedMovies();
+    // getSearchedMovies();
   }, []);
   
   return (
     <div className='container-fluid'>
+      <div className='d-flex align-items-center justify-content-between my-3'>
+        < MovieListHeading heading = {"Movie App"} />
+        < SearchBox />
+      </div>
       <div className="d-flex justify-content-start movie-app-row">
         <MovieList movies = {movies} />
       </div>
---
+++ src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBox = () => {
+  return (
+    <div>
+        <input type="text" className='form-control' placeholder='Type to search' />
+    </div>
+  )
+}
+
+export default SearchBox
