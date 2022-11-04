e87e13b4a00fa149748abba4bc74ee254fcb353e
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-04 15:36:07 -0500

    added removeFavoriteMovie function

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const App = () => {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_DB_Key}&language=en-US&page=1&include_adult=false&query=${searchValue}`;
     const response = await fetch(url);
     const data = await response.json();
+    console.log(data.results);
     // Conditional to check if input has a value
     if(data.results) {
       setMovies(data.results);
@@ -32,21 +33,43 @@ const App = () => {
     const newFavoriteList = [...favorites, movie];
     setFavorites(newFavoriteList);
   }
+
+  const removeFavoriteMovie = (movie) => {
+    const newFavoriteList = favorites.filter(
+        (favorite) => favorite.id !== movie.id
+    );
+    setFavorites(newFavoriteList);
+  }
   
   return (
     <div className='container-fluid'>
         <div className='d-flex align-items-center justify-content-between my-3'>
-            < MovieListHeading heading = {"Movie App"} />
-            < SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+            < MovieListHeading 
+                heading = {"Movie App"} 
+            />
+            < SearchBox 
+                searchValue={searchValue} 
+                setSearchValue={setSearchValue} 
+            />
         </div>
         <div className="d-flex justify-content-start movie-app-row">
-            < MovieList movies = {movies} favComponent={AddFavorites} handleFavoritesClick={addFavoriteMovie} />
+            < MovieList 
+                movies={movies} 
+                favComponent={AddFavorites} 
+                handleFavoritesClick={addFavoriteMovie} 
+            />
         </div>
         <div className='d-flex align-items-center justify-content-between my-3'>
-            < MovieListHeading heading = {"Favorites"} />
+            < MovieListHeading 
+                heading = {"Favorites"} 
+            />
         </div>
         <div className="d-flex justify-content-start movie-app-row">
-            < MovieList movies = {favorites} favComponent={RemoveFavorites} handleFavoritesClick={addFavoriteMovie} />
+            < MovieList 
+                movies = {favorites} 
+                favComponent={RemoveFavorites} 
+                handleFavoritesClick={removeFavoriteMovie} 
+            />
         </div>
     </div>
   );
