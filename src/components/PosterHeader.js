80d10819864220283fb17d53dcc56a0878c8f03c
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-08 14:34:41 -0600

    added and styled rating number for movie

--- src/components/PosterHeader.js
+++ src/components/PosterHeader.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const PosterHeader = (props) => {
   return (
-    
+    <div className='d-flex justify-content-between'>
+        <p className='vote-num'>{props.movie.vote_average}</p>
+        <p>test image</p>
+    </div>
   )
 }
 
