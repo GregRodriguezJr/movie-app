515150d720752ffd8f919a4a0a1739cf0b3eab9f
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2022-12-29 14:57:08 -0600

    able to add a favorite movie to firebase, changed orger of sections

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import MovieList from './components/MovieList';
-import MovieListHeading from './components/MovieListHeading';
-import SearchBox from './components/SearchBox';
-import AddFavorites from './components/AddFavorites';
-import RemoveFavorites from './components/RemoveFavorites';
-import { collection, onSnapshot, query } from 'firebase/firestore';
-import { db } from './firebase';
+import { useEffect, useState } from "react";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddFavoritesIcon from "./components/AddFavoritesIcon";
+import RemoveFavorites from "./components/RemoveFavorites";
+import { addDoc, collection, onSnapshot, query } from "firebase/firestore";
+import { db } from "./firebase";
+import { async } from "@firebase/util";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [favorites, setFavorites] = useState([]);
   const [popularMovies, setPopularMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
   const [favMovies, setFavMovies] = useState([]);
 
   const getSearchedMovies = async (searchValue) => {
@@ -23,10 +24,10 @@ const App = () => {
     const resultsList = data.results;
     // Filter out movies with no poster
     const filteredList = resultsList.filter(
-      item => item.poster_path !== null
+      (item) => item.poster_path !== null
     );
     setMovies(filteredList);
-  }
+  };
 
   // API call to moviedb to get current popular movies
   const getPopularMovies = async () => {
@@ -36,20 +37,20 @@ const App = () => {
     console.log(data.results);
     setPopularMovies(data.results);
     console.log(data.results);
-  }
+  };
 
   // Read favorites from firebase
-  useEffect(()=> {
-    const q = query(collection(db,'favorite_movies'))
-    const unsubscribe = onSnapshot(q, (QuerySnapshot)=> {
-      let favMoviesArr = []
-      QuerySnapshot.forEach((doc)=>{
-        favMoviesArr.push({...doc.data(), id: doc.id})
+  useEffect(() => {
+    const q = query(collection(db, "favorite_movies"));
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let favMoviesArr = [];
+      QuerySnapshot.forEach((doc) => {
+        favMoviesArr.push({ ...doc.data(), id: doc.id });
       });
-      setFavMovies(favMoviesArr)
+      setFavMovies(favMoviesArr);
     });
     return () => unsubscribe;
-  },[])
+  }, []);
 
   // When search value changes getSearchedMovies is ran
   useEffect(() => {
@@ -61,63 +62,62 @@ const App = () => {
     getPopularMovies();
   }, []);
 
-  const addFavoriteMovie = (movie) => {
-    // Conditional to prevent duplicate movies
-    if(favorites.includes(movie)) {
-      return;
-    }
-    const newFavoriteList = [...favorites, movie];
-    setFavorites(newFavoriteList);
-  }
+  // Create new favorite movie in firebase
+  const addFavoriteMovie = async (movie) => {
+    // TODO! Conditional to prevent duplicate movies
+    await addDoc(collection(db, "favorite_movies"), {
+      title: movie.title,
+      overview: movie.overview,
+      vote_average: movie.vote_average,
+      poster_path: movie.poster_path,
+      release_date: movie.release_date,
+    });
+  };
 
   const removeFavoriteMovie = (movie) => {
     const newFavoriteList = favorites.filter(
-        (favorite) => favorite.id !== movie.id
+      (favorite) => favorite.id !== movie.id
     );
     setFavorites(newFavoriteList);
-  }
-  
+  };
+
   return (
-    <div className='container'>
+    <div className="container">
 
       {/* Header */}
-
-      <div className='d-flex align-items-center justify-content-center flex-wrap my-2'>
-        < MovieListHeading heading = {"Movie App"} />
-        < SearchBox 
-          searchValue={searchValue} 
-          setSearchValue={setSearchValue} 
-        />
+      <div className="d-flex align-items-center justify-content-center flex-wrap my-4">
+        <h1 className="m-0">Movie App</h1>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
 
-      {/* Popular Movies Section */} 
-
-      < MovieListHeading heading = {"Popular"} />
-      < MovieList 
-          movies={popularMovies} 
-          favComponent={AddFavorites} 
-          handleFavoritesClick={addFavoriteMovie} 
-      />
-
       {/* Search Results Section */}
-
-      < MovieListHeading heading = {"Search Results"} />
-      < MovieList 
-          movies={movies} 
-          favComponent={AddFavorites} 
-          handleFavoritesClick={addFavoriteMovie} 
+      <MovieListHeading heading={"Search Results"} />
+      <MovieList
+        movies={movies}
+        favComponent={AddFavoritesIcon}
+        handleFavoritesClick={addFavoriteMovie}
       />
+      <hr></hr>
 
       {/* Favorite Movies Section */}
-
-      < MovieListHeading heading = {"Favorites"} />
-      < MovieList 
-          movies = {favMovies} 
-          favComponent={RemoveFavorites} 
-          handleFavoritesClick={removeFavoriteMovie} 
+      <MovieListHeading heading={"Favorites"} />
+      <MovieList
+        movies={favMovies}
+        favComponent={RemoveFavorites}
+        handleFavoritesClick={removeFavoriteMovie}
       />
+      <hr></hr>
+
+      {/* Popular Movies Section */}
+      <MovieListHeading heading={"Popular"} />
+      <MovieList
+        movies={popularMovies}
+        favComponent={AddFavoritesIcon}
+        handleFavoritesClick={addFavoriteMovie}
+      />
+
     </div>
   );
-}
+};
 
 export default App;
