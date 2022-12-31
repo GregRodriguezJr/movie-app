97a85066ddd3dfb7fa8d8236c5bda14a3d5038dc
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2022-12-30 18:55:54 -0600

    added a button to clear search input

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import RemoveFavoritesIcon from "./components/RemoveFavoritesIcon";
 import { db } from "./firebase";
 import { addFavoriteMovie, getFavorites, removeFavoriteMovie } from "./Crud";
 import { getPopularMovies, getSearchedMovies } from "./apiRequest";
+import { Button } from "react-bootstrap";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [popularMovies, setPopularMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState('');
   const [favMovies, setFavMovies] = useState([]);
 
   // API call to moviedb to get current popular movies
@@ -35,13 +36,18 @@ const App = () => {
     getSearchedMovies(searchValue, setMovies);
   }, [searchValue]);
 
+  const clearSearch = () => {
+    setSearchValue("");
+  }
+
   return (
     <div className="container">
 
       {/* Header */}
       <div className="d-flex align-items-center justify-content-center flex-wrap my-4">
         <h1 className="m-0">Movie App</h1>
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+        <SearchBox value={searchValue} setSearchValue={setSearchValue} />
+        <Button className="m-2" onClick={clearSearch}>Clear</Button>
       </div>
 
       {/* Search Results Section */}
